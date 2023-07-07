64adbeb7ba1c8eb3f1fc8c58ea692aa25e488202
Antonio Gamero <125345806+nono343@users.noreply.github.com>
2023-07-07 19:16:26 +0000

    pmv

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -33,8 +33,8 @@ export const Footer = () => {
           </li>
         </ul>
         <Socialnerworksicons />
-        <Subscribe />
-        <Payicons />
+        {/* <Subscribe />
+        <Payicons /> */}
         <div className="text-center text-gray-500 dark:text-gray-200 pt-10 sm:pt-12 font-light flex items-center justify-center">
           Realizado por A. Chiarilli, S. Fernandes, A. Gamero y J. Guerrero
         </div>
