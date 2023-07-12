7fda8554d4af163c64531720048c58d5a9f80ed0
Sofia Fernandes <123645624+sofiafernands@users.noreply.github.com>
2023-07-12 15:55:37 +0000

    correccion de contraoferta

--- src/front/js/component/cardProfilePro.js
+++ src/front/js/component/cardProfilePro.js
@@ -163,22 +163,18 @@ export const Cardprofilepro = (props) => {
                             </div>
 
                             <div className="mt-6 flex justify-end">
-                            <button
-    onClick={() => {
-        submitPost();
-        setShowModal(false);
-    }}
-    className="px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
->
-    Añadir Reserva
-</button>
-
-<button
-    onClick={() => setShowModal(false)}
-    className="px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-gray-700 bg-gray-200 hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 ml-3"
->
-    Cancelar
-</button>
+                                <button
+                                    onClick={submitPost}
+                                    className="px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                                >
+                                    Añadir Reserva
+                                </button>
+                                <button
+                                    onClick={() => setShowModal(false)}
+                                    className="px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-gray-700 bg-gray-200 hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 ml-3"
+                                >
+                                    Cancelar
+                                </button>
                             </div>
                         </div>
                     </div>
