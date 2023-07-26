8a2eb7de41bc014c36912cf6a5468731b5bf90ad
Sofia Fernandes <123645624+sofiafernands@users.noreply.github.com>
2023-07-26 23:12:06 +0000

    modificando flask_env a production

--- migrations/versions/60a68ea036db_.py
+++ migrations/versions/60a68ea036db_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: f08a4b4dfe75
+Revision ID: 60a68ea036db
 Revises: 
-Create Date: 2023-07-26 14:55:59.843376
+Create Date: 2023-07-26 19:55:08.288408
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = 'f08a4b4dfe75'
+revision = '60a68ea036db'
 down_revision = None
 branch_labels = None
 depends_on = None
